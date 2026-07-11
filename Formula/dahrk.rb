a624b616fe9c37ffed6ae9539635aa2c22df6783
dahrk-release <release@dahrk.ai>
2026-07-11 13:00:25 +0000

    dahrk 0.1.9

--- Formula/dahrk.rb
+++ Formula/dahrk.rb
@@ -10,8 +10,8 @@
 class Dahrk < Formula
   desc "Dahrk edge client: run a self-managed node that executes Dahrk workflow stages"
   homepage "https://dahrk.ai"
-  url "https://registry.npmjs.org/dahrk-node/-/dahrk-node-0.1.8.tgz"
-  sha256 "30963d81f8153766ba1018288d04f0fbe5b8c376f980f74a0b2a657534138c20"
+  url "https://registry.npmjs.org/dahrk-node/-/dahrk-node-0.1.9.tgz"
+  sha256 "140e5e1ef68a249a880ee8f549ed9c3ccde4b3e24362ee589bd66e1bff4a3998"
   license "Apache-2.0"
 
   depends_on "node"
